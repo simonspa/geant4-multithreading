059887ea604a3bb58108e55c37bac41dff60cc98
mmoanis <mohamed_moanis@outlook.com>
2019-06-11 17:38:12 +0200

    Seed worker once per BeamOn call

--- SimpleMasterRunManager.cpp
+++ SimpleMasterRunManager.cpp
@@ -42,23 +42,17 @@ void SimpleMasterRunManager::Run(G4int i_event, G4int n_event)
     // seed the run here first before we run on a seperate thread.
     G4RNGHelper* helper = G4RNGHelper::GetInstance();
 
-    for (G4int i = 0; i < n_event; ++i) {
-        G4int idx_rndm = nSeedsPerEvent*nSeedsUsed;
-        long s1 = helper->GetSeed(idx_rndm), s2 = helper->GetSeed(idx_rndm+1);
-        worker_run_manager_->seedsQueue.push(s1);
-        worker_run_manager_->seedsQueue.push(s2);
-        G4cout << "SetUpAnEvent s1=" << s1 << " s2=" << s2 << G4endl;
+    G4int idx_rndm = nSeedsPerEvent*nSeedsUsed;
+    long s1 = helper->GetSeed(idx_rndm), s2 = helper->GetSeed(idx_rndm+1);
+    worker_run_manager_->seedsQueue.push(s1);
+    worker_run_manager_->seedsQueue.push(s2);
+    G4cout << "SetUpAnEvent s1=" << s1 << " s2=" << s2 << G4endl;
 
-        // TODO: check if needed
-        //if(nSeedsPerEvent==3)
-        //    worker_run_manager_->seedsQueue.push(helper->GetSeed(idx_rndm+2));
-
-        nSeedsUsed++;
-        if(nSeedsUsed==nSeedsFilled) {
-            // The RefillSeeds call will refill the array with 1024 new entries
-            numberOfEventToBeProcessed = nSeedsFilled + 1024;
-            RefillSeeds();
-        }
+    nSeedsUsed++;
+    if(nSeedsUsed==nSeedsFilled) {
+        // The RefillSeeds call will refill the array with 1024 new entries
+        numberOfEventToBeProcessed = nSeedsFilled + 1024;
+        RefillSeeds();
     }
 
     numberOfEventProcessed += n_event;
